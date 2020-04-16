762a28e1068e8f2a4757b495fbfdd5c1c87ba6d9
cmentel <58997650+cmentel@users.noreply.github.com>
2020-04-16 17:25:35 -0400

    Update main.c

--- Binary Search Tree/main.c
+++ Binary Search Tree/main.c
@@ -1,22 +1,9 @@
-// Compile this assignment with: gcc main.c -o main
-//
-// Include parts of the C Standard Library
-// These have been written by some other really
-// smart engineers.
+// Compile with: gcc main.c -o main
+
 #include <stdio.h>  // For IO operations
 #include <stdlib.h> // for malloc/free
-
-// Our library that we have written.
-// Also, by a really smart engineer!
 #include "my_bst.h"
-// Note that we are locating this file
-// within the same directory, so we use quotations
-// and provide the path to this file which is within
-// our current directory.
 
-// ====================================================
-// ================== Program Entry ===================
-// ====================================================
 int main() {
 
     // creates BST
